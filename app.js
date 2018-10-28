e5951eac6740b8fdd2e48da284dd601e877c9bf7
swaleeha <tzuleiha@gmail.com>
2018-10-28 18:09:52 +0100

    add cors

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ var createError = require('http-errors');
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
